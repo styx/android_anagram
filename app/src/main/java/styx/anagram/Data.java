d218264289550ef139170aebbab0e38aaf406098
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-07-16 20:14:12 +0300

    Initial variant of data
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

---
+++ app/src/main/java/styx/anagram/Data.java
@@ -0,0 +1,9 @@
+package styx.morse;
+
+import java.util.HashMap;
+
+public class Data {
+    public static final HashMap<String, String> En =
+            new HashMap<String, String>() {{
+            }};
+}

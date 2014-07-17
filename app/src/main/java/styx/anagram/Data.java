3d34e1be75a2824c0c06d39fc8780b45bcb52d00
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-07-17 13:21:24 +0300

    Add basic helper class for DB
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- app/src/main/java/styx/anagram/Data.java
+++ app/src/main/java/styx/anagram/Data.java
@@ -1,9 +1,14 @@
 package styx.anagram;
 
-import java.util.HashMap;
+import android.content.Context;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
-public class Data {
-    public static final HashMap<String, String> Words =
-            new HashMap<String, String>() {{
-            }};
+public class Data extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "data.db.gz";
+    private static final int DATABASE_VERSION = 1;
+
+    public Data(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 }

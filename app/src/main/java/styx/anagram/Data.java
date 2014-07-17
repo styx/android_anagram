98e56486590388dbd5e661fe47de4b8cc9ff6746
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-07-17 16:42:35 +0300

    Search by entity
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- app/src/main/java/styx/anagram/Data.java
+++ app/src/main/java/styx/anagram/Data.java
@@ -1,14 +1,35 @@
 package styx.anagram;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.util.Log;
+
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class Data extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "data.db.gz";
+    private static final String DATABASE_NAME = "data.db";
     private static final int DATABASE_VERSION = 1;
 
     public Data(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+    public Cursor search_by_entity(String request) {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        String[] columns = {"entity", "info"};
+        String where = "entity LIKE '" + request + "'";
+
+        qb.setTables("Dict");
+        Cursor c = qb.query(db, columns, where, null,
+                null, null, null);
+
+        c.moveToFirst();
+        return c;
+    }
+
 }
